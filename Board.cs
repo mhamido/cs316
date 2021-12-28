38c5a506f919e217f6f44e505a760e00c303e4f8
MiltonHs <mahmoudmhamido@gmail.com>
2021-12-28 21:45:06 +0200

    initial commit

---
+++ Board.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Board
+{
+    public int Rows { get; }
+    public int Columns { get; }
+    public int WinningStreak { get; }
+    public Cell[,] Cells { get; }
+    private int movesPlayed;
+
+    public Board(int rows = 6, int columns = 7, int winningStreak = 4)
+    {
+        // The minimum dimensions required
+        // to play Connect-Four is 5x4.
+        movesPlayed = 0;
+        Rows = Math.Max(rows, 4);
+        Columns = Math.Max(columns, 5);
+        WinningStreak = Math.Max(winningStreak, 4);
+        Cells = new Cell[Rows, Columns];
+        
+        for (int row = 0; row < Rows; ++row)
+        {
+            for (int col = 0; col < Columns; ++col)
+            {
+                Cells[row, col] = Cell.Empty;
+            }
+        }
+    }
+
+    public bool MakeMove(Player player, int column)
+    {
+        // ^ True = Player won, False = Player did not win using this move.
+
+        // Ensure column is not full.
+        // Ensure 0 <= column <= Columns
+        ++movesPlayed;
+
+        // Find the row closest to the bottom.
+        int row;
+
+        // Put the player piece in the cells of the board.
+
+        // Check if the player won with the most recent move
+        return HasWon(player, row, column);
+    }
+
+    private bool HasWon(Player player, int mostRecentRow, int mostRecentColumn)
+    {
+        // Check Rows:
+        // (Since rows are filled from bottom to top, it's
+        // faster to search for a winning move from the bottom.)
+
+        // Check Diagonals
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Cell : byte
+{
+    Empty,
+    AgentPiece,
+    HumanPiece,
+}
+
+public enum Player : byte
+{
+    Agent,
+    Human
+}
+
+public interface IPlayer
+{
+    Cell GetCell();
+    Player GetPiece();
+
+    int Deliberate(Board board);
+}
+
+sealed class Agent : IPlayer
+{
+    private int depthLimit = 4;
+
+    public Agent()
+    {
+
+    }
+
+    private IEnumerable<Board> GetSucessors(Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Deliberate(Board board)
+    {
+        int minimax(Board currentBoard, int depth, Player player)
+        {
+            if (depth == 0)
+                return Evaluate(currentBoard);
+
+            switch (player)
+            {
+                case Player.Agent:
+                    int bestOption = Int32.MinValue;
+                    foreach (var Child in currentBoard)
+                    {
+
+                    }
+                    break;
+                case Player.Human:
+                    break;
+            }
+        }
+
+        return minimax(board, depthLimit, Player.Agent);
+    }
+
+    private int Evaluate(Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Cell GetCell() => Cell.AgentPiece;
+    public Player GetPiece() => Player.Agent;
+}
+
+sealed class Human : IPlayer
+{
+    public int Deliberate(Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Cell GetCell() => Cell.HumanPiece;
+    public Player GetPiece() => Player.Human;
+}
